b4d644742cb3bb8407b985df685e3c28c60f3db3
Cesar Ramirez <cramirez@intersysconsulting.com>
2018-11-06 20:30:40 -0600

    initital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/api/usuarios', (req, res) => {
+    res.send({ usuarios: 'wololo'});
+});
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    });
+}
+
+app.listen(process.env.PORT || 5000);
+
+
